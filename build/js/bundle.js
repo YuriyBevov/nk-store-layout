a4e07bfe724766dc6a496109b32938640b8ffc29
Yuriy Bevov <jury19y@mail.ru>
2021-12-25 16:36:59 +0300

    подготовка к работе, начало верстки
    asdf

---
+++ build/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./source/scripts/index.js ***!
+  \*********************************/
+
+
+console.log('script is ok !')
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
